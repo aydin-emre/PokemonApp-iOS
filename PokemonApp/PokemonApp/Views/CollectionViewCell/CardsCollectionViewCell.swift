0060c9890fe5517e661fd608665d2cb5dcfced0c
Emre Huawei <ea@ea.tc>
2022-02-05 14:31:02 +0300

    Bug fixing..

--- PokemonApp/PokemonApp/Views/CollectionViewCell/CardsCollectionViewCell.swift
+++ PokemonApp/PokemonApp/Views/CollectionViewCell/CardsCollectionViewCell.swift
@@ -13,13 +13,9 @@ class CardsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var cardView: CardView!
 
     func configure(with card: Card) {
-        DispatchQueue.global(qos: .background).sync {
-            if let imageURL = card.imageURL,
-               let url = URL(string: imageURL) {
-                DispatchQueue.main.async {
-                    self.cardView.imageView.sd_setImage(with: url, completed: nil)
-                }
-            }
+        if let imageURL = card.imageURL,
+           let url = URL(string: imageURL) {
+            self.cardView.imageView.sd_setImage(with: url, completed: nil)
         }
         cardView.titleLabel.text = card.name
     }
